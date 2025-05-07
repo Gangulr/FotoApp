776e469d53a9c7670b9c079a2df83bcd2831938a
Gangulr <167494975+Gangulr@users.noreply.github.com>
2025-05-07 19:34:24 +0530

    reedit

--- sadeepa-frontend copy/src/features/posts/views/PostCard.jsx
+++ sadeepa-frontend copy/src/features/posts/views/PostCard.jsx
@@ -60,7 +60,7 @@ const PostCard = ({ post, onUpdateClicked, onDeleteClicked }) => {
                   className="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                 >
                   <Pencil className="w-4 h-4 mr-2" />
-                  Edit Post
+                  Edit Posts
                 </button>
                 <button
                   onClick={() => {
