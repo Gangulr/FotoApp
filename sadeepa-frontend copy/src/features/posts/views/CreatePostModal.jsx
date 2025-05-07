e462c6bf645e0a9968bbf32310c5a0b26f7b1402
ChalaRamanayake <pixelnova88@gmail.com>
2025-05-07 08:50:35 +0530

    Update CreatePostModal.jsx
    commit

--- sadeepa-frontend copy/src/features/posts/views/CreatePostModal.jsx
+++ sadeepa-frontend copy/src/features/posts/views/CreatePostModal.jsx
@@ -159,7 +159,7 @@ const CreatePostModal = ({ onRefresh }) => {
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
             <label className="block text-sm font-medium text-gray-700">
-              Titles
+              Title
             </label>
             <input
               type="text"
